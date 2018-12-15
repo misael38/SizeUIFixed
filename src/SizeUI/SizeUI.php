b61c430b7cad9b00ad326d8374f5c339ff876bf5
FazeHamptonApex <45886868+FazeHamptonApex@users.noreply.github.com>
2018-12-15 00:07:48 -0500

    fixed?

--- src/SizeUI/SizeUI.php
+++ src/SizeUI/SizeUI.php
@@ -8,67 +8,67 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 class SizeUI extends PluginBase implements Listener{
-  public function onEnable(): void{
-    $this->getServer()->getPluginManager()->registerEvents(($this), $this);
-    $this->getLogger()->info("SizeUI By FazeHampton Enabled");
-    }
-    
-    public function onDisable(): void{
-      $this->getServer()->getPluginManager()->registerEvents(($this, $this);
-      $this->getLogger()->info("SizeUI By FazeHampton Disabled");
-      }
-      
-      public function checkDepends(): void{
-        $this->formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-        if(is_null($this->formapi)){
-          $this->getLogger()->error("SizeUI Needs FormAPI To Work");
-          $this->getPluginLoader()->disablePlugin($this);
-          }
-       }
-    }
-    
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
-      if($cmd->getName() == "size"){
-        if(!($sender instanceof Player)){
-          $sender->addTitle("SizeUI"), false);
-          return true;
-       }
-       $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-       $form = $api->createSimpleForm(function (Player $sender, $data){
-         $result = $data;
-         if ($result == null){
-           }
-           switch ($result){
-             case 0:
-                 $sender->addTitle("Size Updated)";
-                 $player->setScale("0");
-                 break;
-             case 1:
-                 $sender->addTitle("Size Updated)";
-                 $player->setScale("1");
-                 break;
-             case 2:
-                 $sender->addTitle("Size Updated)";
-                 $player->setScale("2");
-                 break;
-             case 3:
-                 $sender->addTitle("Size Updated");
-                 $player->setScale("3");
-                 break;
-             case 4:
-                 $sender->addTitle("Size Updated");
-                 break;
+          public function onEnable(): void{
+            $this->getServer()->getPluginManager()->registerEvents(($this), $this);
+            $this->getLogger()->info("SizeUI By FazeHampton Enabled");
+            }
+            
+            public function onDisable(): void{
+              $this->getServer()->getPluginManager()->registerEvents(($this, $this);
+              $this->getLogger()->info("SizeUI By FazeHampton Disabled");
+              }
+              
+              public function checkDepends(): void{
+                $this->formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+                if(is_null($this->formapi)){
+                  $this->getLogger()->error("SizeUI Needs FormAPI To Work");
+                  $this->getPluginLoader()->disablePlugin($this);
+                  }
+               }
+            }
+            
+            public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
+              if($cmd->getName() == "size"){
+                if(!($sender instanceof Player)){
+                  $sender->addTitle("SizeUI"), false);
+                  return true;
+               }
+               $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+               $form = $api->createSimpleForm(function (Player $sender, $data){
+                 $result = $data;
+                 if ($result == null){
+                   }
+                   switch ($result){
+                     case 0:
+                         $sender->addTitle("Size Updated)";
+                         $player->setScale("0");
+                         break;
+                     case 1:
+                         $sender->addTitle("Size Updated)";
+                         $player->setScale("1");
+                         break;
+                     case 2:
+                         $sender->addTitle("Size Updated)";
+                         $player->setScale("2");
+                         break;
+                     case 3:
+                         $sender->addTitle("Size Updated");
+                         $player->setScale("3");
+                         break;
+                    case 4:
+                        $sender->addTitle("Size Updated");
+                        break;
+               }
+           });
+           $form->setTitle("SizeUI");
+           $form->setContent("SizeUI");
+           $form->addButton("Mini");
+           $form->addButton("Normal");
+           $form->addButton("Grand");
+           $form->addButton("MegaGrand");
+           $form->addButton("Exit SizeUI");
+           $form->sendToPlayer($sender);
         }
-    });
-    $form->setTitle("SizeUI");
-    $form->setContent("SizeUI");
-    $form->addButton("Mini");
-    $form->addButton("Normal");
-    $form->addButton("Grand");
-    $form->addButton("MegaGrand");
-    $form->addButton("Exit SizeUI");
-    $form->sendToPlayer($sender);
+        return true;
     }
-    return true;
-  }
 }
