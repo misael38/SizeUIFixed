2b04cb8f837ddf8c5c0183ca2919578aa757514c
Misael38 <40845343+misael38@users.noreply.github.com>
2019-01-01 15:59:35 +0800

    Update Main.php

--- src/SizeUI/Main.php
+++ src/SizeUI/Main.php
@@ -42,23 +42,23 @@ class Main extends PluginBase implements Listener{
                 }
                 switch ($result) {
                     case 0:
-                        $player->sendMessage(TextFormat::GREEN . "§7[§eSizeUI§7] §aSize Updated To §b1");
+                        $sender->sendMessage(TextFormat::GREEN . "§7[§eSizeUI§7] §aSize Updated To §b1");
                         $player->$setScale("1");
                         break;
                     case 1:
-                        $player->sendMessage(TextFormat::GREEN . "§7[§eSizeUI§7] §aSize Updated To §b2");
+                        $sender->sendMessage(TextFormat::GREEN . "§7[§eSizeUI§7] §aSize Updated To §b2");
                         $player->$setScale("2");
                         break;
                     case 2:
-                        $player->sendMessage(TextFormat::GREEN . "§7[§eSizeUI§7] §aSize Updated To §b3");
+                        $sender->sendMessage(TextFormat::GREEN . "§7[§eSizeUI§7] §aSize Updated To §b3");
                         $player->$setScale("3");
                         break;
                     case 3:
-                        $player->sendMessage(TextFormat::GREEN . "§7[§eSizeUI§7] §aSize Updated To §b4");
+                        $sender->sendMessage(TextFormat::GREEN . "§7[§eSizeUI§7] §aSize Updated To §b4");
                         $player->$setScale("4");
                         break;
                     case 4:
-                        $player->sendMessage(TextFormat::RED . "§cExiting SizeUI Menu");
+                        $sender->sendMessage(TextFormat::RED . "§cExiting SizeUI Menu");
                         break;
                 }
             });
@@ -69,7 +69,7 @@ class Main extends PluginBase implements Listener{
             $form->addButton("§lBig");
             $form->addButton("§lVery Big");
             $form->addButton("§lBack");
-            $form->sendToPlayer($player);
+            $form->sendToPlayer($sender);
         }
         return true;
     }
