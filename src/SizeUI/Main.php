633866275fa742414b5a72d3fe477b686f2cf36a
Misael38 <40845343+misael38@users.noreply.github.com>
2019-01-03 01:07:19 +0800

    Update Main.php

--- src/SizeUI/Main.php
+++ src/SizeUI/Main.php
@@ -10,7 +10,6 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\server\ServerCommandEvent;
-use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener{
     
